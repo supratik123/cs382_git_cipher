3d9a39ca7a055d1a910a1e1323c49c670b22d7a8
sacusa <sudhanshugupta96@gmail.com>
2018-09-29 15:19:55 +0530

    Add Cipher.java; cipher unimplemented

---
+++ Cipher.java
@@ -0,0 +1,9 @@
+class Cipher {
+    public static String encrypt(String text, int key) {
+        // TODO
+    }
+
+    public static String decrypt(String text, int key) {
+        // TODO
+    }
+}
